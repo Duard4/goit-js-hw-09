06117603e256dd06fc6af5b61871c0f9a7e30fbc
Eduard <parhomenkoeduard75@gmail.com>
2024-10-01 19:21:13 +0300

    form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -3,24 +3,27 @@ const email = form.elements.email;
 const message = form.elements.message;
 const localStorageKey = 'feedback-form-state';
 
-const values = JSON.parse(localStorage.getItem(localStorageKey))
-console.log(values)
+const values = JSON.parse(localStorage.getItem(localStorageKey));
 email.value = values ? values.email : '';
 message.value = values ? values.message : '';
 
-const formData = { email: "", message: "" };
+let formData = { email: '', message: '' };
 
 form.addEventListener('input', evt => {
-    if (evt.target == message)
-        formData.message = evt.target.value;
-    else
-        formData.email = evt.target.value;
-    localStorage.setItem(localStorageKey, JSON.stringify(formData));
+	if (evt.target == message) formData.message = message.value.trim();
+	else formData.email = email.value.trim();
+	localStorage.setItem(localStorageKey, JSON.stringify(formData));
 });
 
 form.addEventListener('submit', evt => {
 	evt.preventDefault();
-	console.log(formData);
-	localStorage.removeItem(localStorageKey);
-	form.reset();
+	if (!(email.value.trim() && email.value.trim()))
+		alert('Fill please all fields');
+	else {
+		console.log(formData);
+        formData.email = '';
+        formData.message = '';
+        localStorage.removeItem(localStorageKey);
+		form.reset();
+	}
 });
